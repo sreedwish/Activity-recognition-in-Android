b3ff1dbde8f53d4e88d839f45c817b0d078c533b
Sreedwish <sreedwishjk@gmail.com>
2018-03-20 12:12:44 +0530

    first change

---
+++ app/src/main/java/com/entraze/activityrecognition/Constants.java
@@ -0,0 +1,14 @@
+package com.entraze.activityrecognition;
+
+/**
+ * Created by signet on 24/02/2018.
+ */
+
+public class Constants {
+
+    public static final String BROADCAST_DETECTED_ACTIVITY = "activity_intent";
+
+    static final long DETECTION_INTERVAL_IN_MILLISECONDS = 3 * 1000;
+
+    public static final int CONFIDENCE = 70;
+}
